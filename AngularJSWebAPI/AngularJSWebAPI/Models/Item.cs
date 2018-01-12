800d099caea3b8495d22d1b848a019c543c947c0
elena-gertsij <gertsij.elena@yandex.ua>
2018-01-12 10:43:10 +0200

    add the Solution

---
+++ AngularJSWebAPI/AngularJSWebAPI/Models/Item.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AngularJSWebAPI.Models
+{
+    public class Item
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "Поле обязательно")]
+        [StringLength(50, ErrorMessage = "Длина не должна превышать 50 символов")]
+        public string Name { get; set; }
+
+        [Display(Name = "Description")]
+        [Required(ErrorMessage = "Поле обязательно")]
+        [StringLength(512, ErrorMessage = "Длина не должна превышать 512 символов")]
+        public string Description { get; set; }
+
+        [Display(Name = "Count")]
+        [Required(ErrorMessage = "Поле обязательно")]
+        public int Count { get; set; }
+    }
+}
---
+++ AngularJSWebAPI/AngularJSWebAPI/Models/ItemContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace AngularJSWebAPI.Models
+{
+    public class ItemContext:DbContext
+    {
+        public ItemContext()
+            : base("DefaultConnection")
+        {
+        }
+       
+        public DbSet<Item> Items { get; set; }
+    }
+}
